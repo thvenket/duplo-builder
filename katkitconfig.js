3a3052a4039392dcbfe92e3e45787d752fdba3db
Venketesan <thvenket@gmail.com>
2017-09-11 06:25:42 +0000

    katkitconfig new zbuilder

--- katkitconfig.js
+++ katkitconfig.js
@@ -9,7 +9,7 @@
 						  "BuildParams":"PHASE=PRE_DEPLOY_BUILD, FOO=BAR",
 						  "Order":0,
 						  "Parallelism":1,
-						  "ContainerImage":"duplodemo/msbuild:v2",
+						  "ContainerImage":"duplocloud/msbuild:v4",
 						  "ContainerPlatform": "5" 
 					   }
 					]
